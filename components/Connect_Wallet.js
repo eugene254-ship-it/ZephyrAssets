737af42026f41884d8dd6080213abb025f3b9db1
Younes <younesgt350@gmail.com>
2023-09-01 12:52:37 +0200

    New tokenized-assets-platform Project

---
+++ components/Connect_Wallet.js
@@ -0,0 +1,13 @@
+import Web3 from'web3';
+import React from 'react'
+
+
+const Connect_Wallet = () => {
+    const web3 = new Web3("https://localhost:3000"); 
+    
+  return (
+    <div>Connect_Wallet</div>
+  )
+}
+
+export default Connect_Wallet
