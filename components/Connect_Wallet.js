33f6ce61fac95c4d2e550f3d8329853e5f3a6b7c
Younes <younesgt350@gmail.com>
2023-09-30 08:59:39 +0200

    changes

--- components/Connect_Wallet.js
+++ components/Connect_Wallet.js
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import { ethers } from 'ethers';
 import React, { useState, useEffect } from 'react';
 
@@ -7,28 +7,28 @@ const Connect_Wallet = () => {
   const [accountAddress, setAccountAddress] = useState('');
   const [accountImage, setAccountImage] = useState('');
 
-  useEffect(() => {
-    async function checkAccount() {
-      if (window.ethereum) {
-        try {
-          const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-          const selectedAddress = accounts[0];
-          console.log('Account is Connected:', selectedAddress);
-          setIsConnected(true);
-          setAccountAddress(selectedAddress);
+  const checkAccount = async () => {
+    
+    if (window.ethereum) {
+      try {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const selectedAddress = accounts[0];
+        console.log('Account is Connected:', selectedAddress);
+        setIsConnected(true);
+        setAccountAddress(selectedAddress);
 
-          // You should replace this with your logic to fetch the associated image for the address.
-          // Example: const imageUrl = await fetchImageForAddress(selectedAddress);
-          const imageUrl = ''; // Replace with the actual image URL
-
-          setAccountImage(imageUrl);
-        } catch (error) {
-          console.error(error);
-        }
+        // You should replace this with your logic to fetch the associated image for the address.
+        const imageUrl = ''; // Replace with the actual image URL
+        setAccountImage(imageUrl);
+      } catch (error) {
+        console.error(error);
       }
     }
+  };
 
-    checkAccount();
+  useEffect(() => {
+    // Comment this out if you don't want to automatically check for account connection
+    // checkAccount();
   }, []);
 
   return (
@@ -40,7 +40,7 @@ const Connect_Wallet = () => {
       ) : (
         <button
           className="bg-blue-500 p-2 text-white hover:bg-cyan-400 active:bg-cyan-700 mx-2 rounded-xl"
-          
+          onClick={checkAccount}
         >
           Connect Wallet
         </button>
