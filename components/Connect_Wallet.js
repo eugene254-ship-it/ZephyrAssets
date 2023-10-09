808341a98bdb704732e730061b238526461747fe
Younes <younesgt350@gmail.com>
2023-10-09 09:50:38 +0200

    added web3modal

--- components/Connect_Wallet.js
+++ components/Connect_Wallet.js
@@ -1,34 +1,46 @@
 'use client';
-import { ethers } from 'ethers';
 import React, { useState, useEffect } from 'react';
+import Web3Modal from "web3modal";
+import WalletConnectProvider from "@walletconnect/web3-provider";
 
 const Connect_Wallet = () => {
   const [isConnected, setIsConnected] = useState(false);
   const [accountAddress, setAccountAddress] = useState('');
-  const [accountImage, setAccountImage] = useState('');
 
-  const checkAccount = async () => {
-    console.log("Checking account...");
-    
-    if (window.ethereum) {
-      console.log("window.ethereum is available");
-      
-      try {
-        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-        const selectedAddress = accounts[0];
-        console.log('Account is Connected:', selectedAddress);
-        setIsConnected(true);
-        setAccountAddress(selectedAddress);
-      } catch (error) {
-        console.log("An error occurred:", error);
+  let web3Modal;
+
+  const providerOptions = {
+    walletconnect: {
+      package: WalletConnectProvider,
+      options: {
+        infuraId: "822d31f00fbf44048318b8a7bc4c4098"  // Insert your Infura Project ID here
       }
-    } else {
-      console.log("window.ethereum is not available");
     }
   };
-  
+
+  web3Modal = new Web3Modal({
+    network: "mainnet",
+    cacheProvider: true,
+    providerOptions
+  });
+
+  const connectWallet = async () => {
+    const provider = await web3Modal.connect();
+    const web3 = new ethers.providers.Web3Provider(provider);
+    const signer = web3.getSigner();
+    const address = await signer.getAddress();
+    
+    setIsConnected(true);
+    setAccountAddress(address);
+  };
+
+  const checkAccount = async () => {
+    if (web3Modal.cachedProvider) {
+      connectWallet();
+    }
+  };
+
   useEffect(() => {
-    // Comment this out if you don't want to automatically check for account connection
     checkAccount();
   }, []);
 
@@ -36,12 +48,14 @@ const Connect_Wallet = () => {
     <div className="flex items-center space-x-2">
       {isConnected ? (
         <>
-          <span className="text-white ring-2 p-2 rounded-xl ring-black bg-red-700 hover:bg-blue-300">{accountAddress.slice(0, 6)}...</span>
+          <span className="text-white ring-2 p-2 rounded-xl ring-black bg-red-700 hover:bg-blue-300">
+            {accountAddress.slice(0, 6)}...
+          </span>
         </>
       ) : (
         <button
           className="bg-blue-500 p-2 text-white hover:bg-cyan-400 active:bg-cyan-700 mx-2 rounded-xl"
-          onClick={checkAccount}
+          onClick={connectWallet}
         >
           Connect Wallet
         </button>
