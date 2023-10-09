62e2ab334e2f7fe48279cc8d74778441e9a312db
Younes <younesgt350@gmail.com>
2023-10-09 13:12:15 +0200

    README and configs

--- ethereum/hardhat.config.js
+++ ethereum/hardhat.config.js
@@ -3,8 +3,14 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
-
   network: {
+    localhost:{
+      url: "http://127.0.0.1:8545/",
+      chainId: 6845,
+      gas: "auto",
+    }
+    
+    
 
   }
 };
