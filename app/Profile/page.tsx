e226643fbb954e351ecfd329eb123d908057c6c7
Younes <younesgt350@gmail.com>
2023-12-17 21:03:54 +0100

    working on profile dashboard

--- app/Profile/page.tsx
+++ app/Profile/page.tsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React from "react";
+// components/ProfilePage.tsx
 
-const page = () => {
+const user = {
+  name: "Joey",
+  email: "gmail.com"
+
+};
+
+const asset = {
+  title: "Real Estate Property",
+  description: "A beautiful beachfront property.",
+  // Additional asset properties
+};
+
+// Mock transactions data (array of transactions)
+const transactions = [
+  { id: 1, type: "Buy", amount: "2 ETH", date: "2021-01-01" },
+  // More transactions...
+];
+export default function ProfilePage({ user, asset, transactions, analytics }) {
   return (
-    <div>page</div>
-  )
-}
+    <div className="container mx-auto p-6">
+      {/* User Profile Section */}
+      <div className="bg-white shadow rounded-lg p-6 mb-6">
+        <h2 className="text-xl font-bold mb-4">User Profile</h2>
+        <p><strong>Name:</strong> {user.name}</p>
+        <p><strong>Email:</strong> {user.email}</p>
+        {/* Additional user info here */}
+      </div>
 
-export default page
+      {/* Asset Details Section */}
+      <div className="bg-white shadow rounded-lg p-6 mb-6">
+        <h2 className="text-xl font-bold mb-4">Asset Details</h2>
+        <p><strong>Title:</strong> {asset.title}</p>
+        <p><strong>Description:</strong> {asset.description}</p>
+        {/* Additional asset details here */}
+      </div>
+
+      {/* Ownership History Section */}
+      <div className="bg-white shadow rounded-lg p-6 mb-6">
+        <h2 className="text-xl font-bold mb-4">Ownership History</h2>
+        {/* Ownership history details here */}
+      </div>
+
+      {/* Tokenization Process Section */}
+      <div className="bg-white shadow rounded-lg p-6 mb-6">
+        <h2 className="text-xl font-bold mb-4">Tokenization Process</h2>
+        {/* Tokenization process details here */}
+      </div>
+
+      {/* Recent Transactions Table */}
+      <div className="bg-white shadow rounded-lg p-6 mb-6">
+        <h2 className="text-xl font-bold mb-4">Recent Transactions</h2>
+        {/* Transactions table here */}
+      </div>
+
+      {/* Interactive Analytics Charts */}
+      <div className="bg-white shadow rounded-lg p-6 mb-6">
+        <h2 className="text-xl font-bold mb-4">Analytics</h2>
+        {/* Analytics charts here */}
+      </div>
+    </div>
+  );
+}
--- app/Tips/page.tsx
+++ app/Tips/page.tsx
@@ -26,13 +26,15 @@ const page = () => {
                 href="#"
                 className="inline-block rounded-full bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
               >
-                Start Here
+                Start Below
               </a>
             </div>
           </div>
         </div>
       </section>
-      <section></section>
+      <section>
+        
+      </section>
     </FadeUp>
   );
 };
